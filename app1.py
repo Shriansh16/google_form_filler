846c876226f9db02d80023ec30d1997b74d98a53
Shriansh16 <shrianshsingh20@gmail.com>
2024-09-05 21:58:41 +0530

    updated

--- app1.py
+++ app1.py
@@ -1,25 +1,17 @@
 import os
 import streamlit as st
-from dotenv import load_dotenv
-from streamlit_chat import message
-from langchain.chains import ConversationChain
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.prompts import (
-    SystemMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-    ChatPromptTemplate,
-    MessagesPlaceholder
-)
 from data_scrapper import get_google_form_questions
-from langchain_openai import ChatOpenAI
+from openai import OpenAI
+client = OpenAI(api_key=st.secrets["OPENAI_API_KEY"])
 
-# Load environment variables
-load_dotenv()
-
-# Securely retrieve the API key
-#api_key = os.getenv("OPENAI_API_KEY") or st.secrets.get("OPENAI_API_KEY")
-llm = ChatOpenAI(api_key="", model="gpt-3.5-turbo")
-
+def get_response(input):
+    response = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[{"role": "user", "content": input}] ,
+    max_tokens=150,
+    n=1
+    )
+    return response.choices[0].message.content
 # Streamlit setup
 st.write("Please enter the link below:")
 
@@ -29,13 +21,13 @@ user_link = st.text_input("Paste your Google Form link here")
 # Check if the link is provided
 result=None
 if user_link:
-    if st.button("Submit"):
+
         try:
             # Call the function to get Google Form questions and store the returned value
             result = get_google_form_questions(user_link)
             if result:
                 st.write("Google Form Questions:")
-                st.json(result)
+                st.write(result)
             else:
                 st.write("Failed to retrieve questions from the Google Form. Please check the link and try again.")
         except Exception as e:
@@ -43,17 +35,18 @@ if user_link:
 
 # Check if the result is available for extraction
 para = st.text_area("Enter all the information which you want to fill in the Google Form")
-
-if para:
-        try:
-            prompt = (
+submit1 = st.button("Submit",key="12")
+if submit1:
+        prompt = (
                 f"Please extract the following information from the paragraph provided by the user and return only the key-value pairs. "
                 f"Do not include any other words or explanations.\n\n"
                 f"Paragraph provided:\n{para}\n\n"
                 f"Informations to be extracted:\n{result}"
             )
-            st.write("Extracted Information (Key-Value Pairs):")
-            response = llm(prompt)
-            st.write(response)
-        except Exception as e:
-            st.error(f"An error occurred during extraction: {e}")
+        #input_prompt = prompt.format(para=para, result=result)
+        st.write(prompt)
+        #st.write("Extracted Information (Key-Value Pairs):")
+        response = get_response(prompt)
+        st.write(response)
+        
+            
